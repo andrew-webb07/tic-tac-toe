f9d9c5a5a7eb7f512225c4f2d366c2c6554682e1
Andrew Webb <webba@ihopu.org>
2021-12-28 15:18:01 -0600

    styled winner boxes

--- src/components/TicTacToeGrid.js
+++ src/components/TicTacToeGrid.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./TicTacToe.css"
 
 export const TicTacToeGrid = () => {
+    let winnerArray = []
 
     const calculateWinner = (squares) => {
         // All possible winning square combinations
@@ -20,20 +21,23 @@ export const TicTacToeGrid = () => {
         for (let i = 0; i < lines.length; i++) {
           const [a, b, c] = lines[i];
           if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            winnerArray = [a, b, c]
             return squares[a];
           }
         }
+
         // if no match, return null and continue game
         return null;
       }
 
     // create array of 9 items initially with null values
     const [choicesArray, setChoicesArray] = useState(Array(9).fill(null))
-
+    // const [winnerArray, setWinnerArray] = useState([])
     const [xIsNext, setXisNext] = useState(true);
     const winner = calculateWinner(choicesArray)
     const xO = xIsNext ? "X" : "O";
     let counter = 0
+    console.log(winnerArray)
 
     const handleClick = (i) => {
         // create a copy of current choices Array
@@ -56,10 +60,15 @@ export const TicTacToeGrid = () => {
         }
       }
 
+    console.log(choicesArray)
     return (
         <>
             <div className="board">
-                {choicesArray.map((square, i) => (<button className='squares' onClick={() => handleClick(i)}>{square}</button>))}
+                {choicesArray.map((square, i) => (
+                  <>
+                  {winnerArray.length !== 0 && winnerArray.includes(i) ? <button className='winnnerSquares' onClick={() => handleClick(i)}>{square}</button> : <button className='squares' onClick={() => handleClick(i)}>{square}</button>}
+                  </>
+                  ))}
             </div>
             {winner ? <><h1>Winner is {winner}!!!!</h1> <div className="restartButtonWrapper"><button onClick={() => window.location.reload(false)} className="restartButton">Play Again??</button></div></> : counter === 9 ? <><h1>Cats game!</h1><div className="restartButtonWrapper"><button onClick={() => window.location.reload(false)} className="restartButton">Play Again??</button></div> </>: <h1>Next Player: {xO}</h1>}
         </>
